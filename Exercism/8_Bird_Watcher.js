ff11f9dd880f44f220420ecf0ab9715061730fa1
Harshalingole <harshal.ingole21@gmail.com>
2022-06-14 22:34:42 +0530

    Concept: Increment /Decrement & For Loop

---
+++ Exercism/8_Bird_Watcher.js
@@ -0,0 +1,90 @@
+/*
+Instructions
+You are an avid bird watcher that keeps track of how many birds have visited your garden. Usually, you use a tally in a notebook to count the birds but you want to better work with your data. You already digitalized the bird counts per day for the past weeks that you kept in the notebook.
+
+Now you want to determine the total number of birds that you counted, calculate the bird count for a specific week and correct a counting mistake.
+
+Note
+To practice, use a for loop solve each of the tasks below.
+
+1. Determine the total number of birds that you counted so far
+Let us start analyzing the data by getting a high-level view. Find out how many birds you counted in total since you started your logs.
+
+Implement a function totalBirdCount that accepts an array that contains the bird count per day. It should return the total number of birds that you counted.
+
+birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+totalBirdCount(birdsPerDay);
+// => 34
+2. Calculate the number of visiting birds in a specific week
+Now that you got a general feel for your bird count numbers, you want to make a more fine-grained analysis.
+
+Implement a function birdsInWeek that accepts an array of bird counts per day and a week number. It returns the total number of birds that you counted in that specific week. You can assume weeks are always tracked completely.
+
+birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+birdsInWeek(birdsPerDay, 2);
+// => 12
+3. Fix a counting mistake
+You realized that all the time you were trying to keep track of the birds, there was one hiding in a far corner of the garden. You figured out that this bird always spent every second day in your garden. You do not know exactly where it was in between those days but definitely not in your garden. Your bird watcher intuition also tells you that the bird was in your garden on the first day that you tracked in your list.
+
+Given this new information, write a function fixBirdCountLog that takes an array of birds counted per day as an argument. It should correct the counting mistake and return the modified array.
+
+birdsPerDay = [2, 5, 0, 7, 4, 1];
+fixBirdCountLog(birdsPerDay);
+// => [3, 5, 1, 7, 5, 1]
+*/
+
+// 1
+const birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+export function totalBirdCount(birdsPerDay) {
+  let sum = 0;
+  console.log(birdsPerDay.length);
+  for (let ind = 0; ind < birdsPerDay.length; ind++) {
+    sum += birdsPerDay[ind];
+  }
+  return sum;
+}
+console.log(totalBirdCount(birdsPerDay));
+// => 34
+
+// 2
+export function birdsInWeek(birdsPerDay, week) {
+  let sum = 0;
+  // 1 => 0 || 2 =>
+  // 7*1 - 7
+  for (let ind = 7 * week - 7; ind < 7 * week; ind++) {
+    ind;
+    sum += birdsPerDay[ind];
+  }
+  return sum;
+}
+console.log(birdsInWeek(birdsPerDay, 2));
+// => 12
+
+// 3
+// let birdsPerDays = [2, 5, 0, 7, 4, 1];
+// let birdsPerDays = [4, 2];
+const birdsPerDays = [
+  2, 8, 4, 1, 3, 5, 0, 4, 1, 6, 0, 3, 0, 1, 5, 4, 1, 1, 2, 6,
+];
+console.log(birdsPerDays.length);
+export function fixBirdCountLog(birdsPerDay) {
+  for (let ind = 0; ind < birdsPerDays.length; ind++) {
+    if (2 * ind < birdsPerDay.length) {
+      birdsPerDay[2 * ind] = birdsPerDay[2 * ind] + 1;
+    }
+  }
+  return birdsPerDay;
+  console.log(birdsPerDay);
+}
+// const expected = [3, 8, 5, 1, 4, 5, 1, 4, 2, 6, 1, 3, 1, 1, 6, 4, 2, 1, 3, 6];
+console.log(fixBirdCountLog(birdsPerDays));
+// const expected = [3, 8, 5, 1, 4, 5, 1, 4, 2, 6, 1, 3, 1, 1, 6, 4, 2, 1, 3, 6];
+// => [3, 5, 1, 7, 5, 1]
+
+// 3
+export function fiexBirdCounting1(birdsPerDay) {
+  for (let ind = 0; ind < array.length; ind += 2) {
+    birdsPerDay[i] += 1;
+  }
+  return birdsPerDay;
+}
